226959b7404e234ce970f1129f75ef253ce54ef9
ahmdtalaat <ahmd.talat95@gmail.com>
2019-03-16 12:35:48 +0200

    set database

--- Pain_Killer/settings.py
+++ Pain_Killer/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Pain_Killer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'slappydb',
+        'USER': 'blogroot',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
